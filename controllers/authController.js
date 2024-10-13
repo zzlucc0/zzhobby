70fa811ba18058a94dab264798531678531434b2
zzlucc0 <zzlucc0@gmail.com>
2024-10-12 22:02:50 -0700

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,7 @@
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-
+ 
 exports.register = async (req, res) => {
     const { username, email, password } = req.body;
   
